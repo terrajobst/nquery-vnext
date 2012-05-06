59a7e36a3b6bc3d360d4345254bcca79dfeb32d3
Immo Landwerth <immo@landwerth.net>
2012-05-06 11:09:20 -0700

    Fix performance issue when computing Span and FullSpan
    We compute the span Span and FullSpan multiple times which could result
    in O(n^2) complexity due to the tree structure.

--- NQuery.Language/SyntaxNode.cs
+++ NQuery.Language/SyntaxNode.cs
@@ -6,7 +6,30 @@ namespace NQuery.Language
 {
     public abstract class SyntaxNode
     {
+        private TextSpan? _span;
+        private TextSpan? _fullSpan;
+
         public abstract IEnumerable<SyntaxNodeOrToken> GetChildren();
+
+        private TextSpan ComputeSpan()
+        {
+            if (!GetChildren().Any())
+                return new TextSpan(0, 0);
+            
+            var start = GetChildren().First().Span.Start;
+            var end = GetChildren().Last().Span.End;
+            return TextSpan.FromBounds(start, end);
+        }
+
+        private TextSpan ComputeFullSpan()
+        {
+            if (!GetChildren().Any())
+                return new TextSpan(0, 0);
+            
+            var start = GetChildren().First().FullSpan.Start;
+            var end = GetChildren().Last().FullSpan.End;
+            return TextSpan.FromBounds(start, end);
+        }
 
         public abstract SyntaxKind Kind { get; }
 
@@ -14,12 +37,10 @@ namespace NQuery.Language
         {
             get
             {
-                if (!GetChildren().Any())
-                    return new TextSpan(0, 0);
+                if (_span == null)
+                    _span = ComputeSpan();
 
-                var start = GetChildren().First().Span.Start;
-                var end = GetChildren().Last().Span.End;
-                return TextSpan.FromBounds(start, end);
+                return _span.Value;
             }
         }
 
@@ -27,12 +48,10 @@ namespace NQuery.Language
         {
             get
             {
-                if (!GetChildren().Any())
-                    return new TextSpan(0, 0);
+                if (_fullSpan == null)
+                    _fullSpan = ComputeFullSpan();
 
-                var start = GetChildren().First().FullSpan.Start;
-                var end = GetChildren().Last().FullSpan.End;
-                return TextSpan.FromBounds(start, end);
+                return _fullSpan.Value;
             }
         }
     }
