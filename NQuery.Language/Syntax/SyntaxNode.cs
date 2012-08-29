ebd3c8b31821079a7a79a4f7772c2a00d74bf754
Immo Landwerth <immo@landwerth.net>
2012-08-28 23:56:53 -0700

    Add basic navigation support for SyntaxNodes

--- NQuery.Language/Syntax/SyntaxNode.cs
+++ NQuery.Language/Syntax/SyntaxNode.cs
@@ -8,8 +8,6 @@ namespace NQuery.Language
         private TextSpan? _span;
         private TextSpan? _fullSpan;
 
-        public abstract IEnumerable<SyntaxNodeOrToken> ChildNodesAndTokens(); 
-
         private TextSpan ComputeSpan()
         {
             if (!ChildNodesAndTokens().Any())
@@ -28,6 +26,67 @@ namespace NQuery.Language
             var start = ChildNodesAndTokens().First().FullSpan.Start;
             var end = ChildNodesAndTokens().Last().FullSpan.End;
             return TextSpan.FromBounds(start, end);
+        }
+
+        public abstract IEnumerable<SyntaxNodeOrToken> ChildNodesAndTokens();
+
+        public IEnumerable<SyntaxNode> ChildNodes()
+        {
+            return from n in ChildNodesAndTokens()
+                   where n.IsNode
+                   select n.AsNode();
+        }
+
+        public IEnumerable<SyntaxNode> DescendantNodes()
+        {
+            return DescendantNodesAndSelf().Skip(1);
+        }
+
+        public IEnumerable<SyntaxNode> DescendantNodesAndSelf()
+        {
+            return from n in DescendantNodesAndTokensAndSelf()
+                   where n.IsNode
+                   select n.AsNode();
+        }
+        
+        public IEnumerable<SyntaxNodeOrToken> DescendantNodesAndTokens()
+        {
+            return DescendantNodesAndTokensAndSelf().Skip(1);
+        }
+        
+        public IEnumerable<SyntaxNodeOrToken> DescendantNodesAndTokensAndSelf()
+        {
+            var stack = new Stack<SyntaxNodeOrToken>();
+            stack.Push(this);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                yield return current;
+
+                if (!current.IsNode)
+                    continue;
+
+                foreach (var child in current.AsNode().ChildNodesAndTokens().Reverse())
+                    stack.Push(child);
+            }
+        }
+
+        public IEnumerable<SyntaxToken> DescendantTokens()
+        {
+            return from n in DescendantNodesAndTokens()
+                   where n.IsToken
+                   select n.AsToken();
+        }
+
+        public SyntaxToken FirstToken()
+        {
+            return DescendantTokens().First();
+        }
+
+        public SyntaxToken LastToken()
+        {
+            return DescendantTokens().Last();
         }
 
         public abstract SyntaxKind Kind { get; }
