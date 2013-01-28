a9a2e14ddcf6c04a78892efee46891dd311219df
Immo Landwerth <immo@landwerth.net>
2013-01-27 19:03:04 -0800

    Add test to check the semantic model returns symbols for SELECT *

--- src/NQuery.UnitTests/SelectStarTests.cs
+++ src/NQuery.UnitTests/SelectStarTests.cs
@@ -30,5 +30,97 @@ namespace NQuery.UnitTests
 
             Assert.AreEqual(0, diagnostics.Length);
         }
+
+        [TestMethod]
+        public void SelectStar_HasAssociatedTable()
+        {
+            var syntaxTree = SyntaxTree.ParseQuery("SELECT t.* FROM Table t");
+            var compilation = Compilation.Empty.WithSyntaxTree(syntaxTree).WithIdNameTable();
+            var selectStar = compilation.SyntaxTree.Root
+                                        .DescendantNodes()
+                                        .OfType<WildcardSelectColumnSyntax>()
+                                        .Single();
+            var tableInstance = compilation.SyntaxTree.Root
+                                           .DescendantNodes()
+                                           .OfType<NamedTableReferenceSyntax>()
+                                           .Single();
+            var semanticModel = compilation.GetSemanticModel();
+            var diagnostics = semanticModel.GetDiagnostics().ToArray();
+
+            var tableReferenceSymbol = semanticModel.GetDeclaredSymbol(tableInstance);
+            var selectStarSymbol = semanticModel.GetTableInstance(selectStar);
+
+            Assert.AreEqual(0, diagnostics.Length);
+            Assert.AreEqual(tableReferenceSymbol, selectStarSymbol);
+        }
+
+        [TestMethod]
+        public void SelectStar_HasAssociatedTable_UnlessNoTableIsSpecified()
+        {
+            var syntaxTree = SyntaxTree.ParseQuery("SELECT * FROM Table t");
+            var compilation = Compilation.Empty.WithSyntaxTree(syntaxTree).WithIdNameTable();
+            var selectStar = compilation.SyntaxTree.Root
+                                        .DescendantNodes()
+                                        .OfType<WildcardSelectColumnSyntax>()
+                                        .Single();
+            var semanticModel = compilation.GetSemanticModel();
+            var diagnostics = semanticModel.GetDiagnostics().ToArray();
+
+            var selectStarSymbol = semanticModel.GetTableInstance(selectStar);
+
+            Assert.AreEqual(0, diagnostics.Length);
+            Assert.AreEqual(null, selectStarSymbol);
+        }
+
+        [TestMethod]
+        public void SelectStar_HasAssociatedTableColumns()
+        {
+            var syntaxTree = SyntaxTree.ParseQuery("SELECT * FROM Table");
+            var compilation = Compilation.Empty.WithSyntaxTree(syntaxTree).WithIdNameTable();
+            var selectStar = compilation.SyntaxTree.Root
+                                        .DescendantNodes()
+                                        .OfType<WildcardSelectColumnSyntax>()
+                                        .Single();
+            var tableInstance = compilation.SyntaxTree.Root
+                                           .DescendantNodes()
+                                           .OfType<NamedTableReferenceSyntax>()
+                                           .Single();
+            var semanticModel = compilation.GetSemanticModel();
+            var diagnostics = semanticModel.GetDiagnostics().ToArray();
+
+            var tableSymbols = semanticModel.GetDeclaredSymbol(tableInstance).ColumnInstances;
+            var selectStartSymbols = semanticModel.GetColumnInstances(selectStar).ToArray();
+
+            Assert.AreEqual(0, diagnostics.Length);
+            CollectionAssert.AreEqual(tableSymbols, selectStartSymbols);
+        }
+
+        [TestMethod]
+        public void SelectStar_HasAssociatedQueryTableColumns()
+        {
+            var syntaxTree = SyntaxTree.ParseQuery("SELECT * FROM Table");
+            var compilation = Compilation.Empty.WithSyntaxTree(syntaxTree).WithIdNameTable();
+            var selectStar = compilation.SyntaxTree.Root
+                                        .DescendantNodes()
+                                        .OfType<WildcardSelectColumnSyntax>()
+                                        .Single();
+            var tableInstance = compilation.SyntaxTree.Root
+                                           .DescendantNodes()
+                                           .OfType<NamedTableReferenceSyntax>()
+                                           .Single();
+            var semanticModel = compilation.GetSemanticModel();
+            var diagnostics = semanticModel.GetDiagnostics().ToArray();
+
+            var tableColumnNames = semanticModel.GetDeclaredSymbol(tableInstance)
+                                                .ColumnInstances
+                                                .Select(c => c.Name)
+                                                .ToArray();
+            var selectStarColumnNames = semanticModel.GetDeclaredSymbols(selectStar)
+                                                     .Select(qc => qc.Name)
+                                                     .ToArray();
+
+            Assert.AreEqual(0, diagnostics.Length);
+            CollectionAssert.AreEqual(tableColumnNames, selectStarColumnNames);
+        }
     }
 }
