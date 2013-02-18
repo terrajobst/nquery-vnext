fcf67111268c20411368f716c79657b180e03b15
Immo Landwerth <immo@landwerth.net>
2013-02-17 17:27:41 -0800

    Extend build process to centralize signing and assembly metadata
    Inject properties and item groups so that we can
    
    - ensure all projects are signed
    - ensure CommonAssemblyInfo.cs is included by all projects

---
+++ src/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly : AssemblyCopyright("Copyright © Immo Landwerth")]
+[assembly : AssemblyCompany("NQuery")]
+[assembly : AssemblyProduct("NQuery")]
+
+[assembly : CLSCompliant(false)]
+[assembly : ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/NQuery.UnitTests/Properties/AssemblyInfo.cs
+++ src/NQuery.UnitTests/Properties/AssemblyInfo.cs
@@ -1,35 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NQuery.Language.UnitTests")]
+[assembly: AssemblyTitle("NQuery.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NQuery.Language.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ac9f8e4c-9227-4192-b953-a7b5231e7827")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
