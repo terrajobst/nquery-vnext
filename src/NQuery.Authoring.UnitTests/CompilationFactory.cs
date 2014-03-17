35c6a477b10a1e8d62f1660ac87f3f2ec04f3f36
Immo Landwerth <immo@landwerth.net>
2014-03-16 18:02:40 -0700

    Add a test helper that allows marking spans in the query text
    Include marking positions using a pipe:
    
        SELECT e.First|Name
        FROM   Employees e
    
    as well as spans using braces:
    
        SELECT e.{FirstName}
        FROM   Employees e
    
    This will simplify authoring unit tests.

--- src/NQuery.Authoring.UnitTests/CompilationFactory.cs
+++ src/NQuery.Authoring.UnitTests/CompilationFactory.cs
@@ -1,6 +1,7 @@
 using System;
 
 using NQuery.Data.Samples;
+using NQuery.Text;
 
 namespace NQuery.Authoring.UnitTests
 {
@@ -10,18 +11,30 @@ namespace NQuery.Authoring.UnitTests
 
         public static Compilation CreateQuery(string query)
         {
-            int position;
-            return CreateQuery(query, out position);
+            var syntaxTree = SyntaxTree.ParseQuery(query);
+            return new Compilation(DataContext, syntaxTree);
         }
 
         public static Compilation CreateQuery(string query, out int position)
         {
             position = query.IndexOf('|');
-            if (position >= 0)
-                query = query.Remove(position, 1);
+            if (position < 0)
+                throw new ArgumentException("The position must be marked with a pipe, such as 'SELECT e.Empl|oyeeId'");
 
-            var syntaxTree = SyntaxTree.ParseQuery(query);
-            return new Compilation(DataContext, syntaxTree);
+            query = query.Remove(position, 1);
+            return CreateQuery(query);
+        }
+
+        public static Compilation CreateQuery(string query, out TextSpan span)
+        {
+            var start = query.IndexOf('{');
+            var end = query.IndexOf('}') - 1;
+            if (start < 0 || end < 0)
+                throw new ArgumentException("The span must be marked with braces, such as 'SELECT {e.EmployeeId}'");
+
+            span = TextSpan.FromBounds(start, end);
+            query = query.Remove(start, 1).Remove(end, 1);
+            return CreateQuery(query);
         }
     }
 }
