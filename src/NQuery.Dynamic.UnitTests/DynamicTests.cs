f1c3002710d679bf78ed9e38c2fc91889497c00d
Immo Landwerth <immo@landwerth.net>
2014-03-29 11:50:28 -0700

    Add unit test project for NQuery.Dynamic

---
+++ src/NQuery.Dynamic.UnitTests/DynamicTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NQuery.Data.Samples;
+
+namespace NQuery.Dynamic.UnitTests
+{
+    [TestClass]
+    public class DynamicTests
+    {
+        [TestMethod]
+        public void Dynamic_ExecuteDynamicSequenceAllowsLateBoundAccess()
+        {
+            var text = @"
+                SELECT  c.CategoryID,
+                        c.CategoryName
+                FROM    Categories c
+                WHERE   c.CategoryID < 3
+                ORDER   BY 1
+            ";
+
+            var dataContext = DataContextFactory.CreateNorthwind();
+            var query = new Query(dataContext, text);
+
+            var rows = query.ExecuteDynamicSequence().ToArray();
+
+            Assert.AreEqual(1, rows[0].CategoryID);
+            Assert.AreEqual("Beverages", rows[0].CategoryName);
+
+            Assert.AreEqual(2, rows[1].CategoryID);
+            Assert.AreEqual("Condiments", rows[1].CategoryName);
+        }
+    }
+}
