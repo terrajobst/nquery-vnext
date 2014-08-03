a537de4e72ace9a35515e288233ab8663d6dd834
Immo Landwerth <immo@landwerth.net>
2014-08-03 14:54:43 -0700

    Enable package restore in command line builds

--- build.cmd
+++ build.cmd
@@ -1,4 +1,16 @@
 @ECHO OFF
+SETLOCAL
+
+:: Ensure nuget.exe exists. If not, dowload it.
+
+SET CACHED_NUGET=%~dp0NuGet.exe
+IF EXIST %CACHED_NUGET% GOTO RESTORE
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:: Restore NuGet packages
+
+:RESTORE
+.\nuget.exe restore
 
 :: Note: We've disabled node reuse because it causes file locking issues.
 ::       The issue is that we extend the build with our own targets which
