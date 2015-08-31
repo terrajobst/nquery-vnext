16b13b9eb38c2f2fc0a86a9676207cd14a92e321
Immo Landwerth <immo@landwerth.net>
2015-08-30 21:13:39 -0700

    Move msbuild.log to bin folder
    All our build outputs should should go under bin. This makes CI drops
    a tiny bit easier.

--- build.cmd
+++ build.cmd
@@ -1,8 +1,10 @@
 @ECHO OFF
 
+if not exist bin mkdir bin
+
 :: Note: We've disabled node reuse because it causes file locking issues.
 ::       The issue is that we extend the build with our own targets which
 ::       means that that rebuilding cannot successfully delete the task
 ::       assembly.
 
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /nologo /m /v:m /nr:false /flp:verbosity=normal %*
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /nologo /m /v:m /nr:false /flp:verbosity=normal;LogFile=bin\msbuild.log %*
