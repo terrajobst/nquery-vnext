0807662697092e297613a95dee233beccf7b41eb
Immo Landwerth <immo@landwerth.net>
2015-09-01 18:16:48 -0700

    WIP: Add tool to generate grammar from the SyntaxNode APIs
    For documentation purposes we need to define a grammar. One option is
    to hand write this from the parser. The other option to tool a baseline
    from the SyntaxNode APIs.
    
    Ideally, our grammar would be a bit more detailed though, in order to
    get precedences and operator priorities across. However, that's hard
    to tool because that would require inferring that information from the
    structure of our recursive decent parser.

---
+++ src/ConsoleApplication1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using NQuery;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var assembly = typeof (SyntaxNode).Assembly;
+            var nodes = assembly.GetExportedTypes().Where(t => typeof (SyntaxNode).IsAssignableFrom(t)).ToArray();
+
+            var typesByBaseType = nodes.ToLookup(n => n.BaseType);
+            var roots = nodes.Where(n => n != typeof(SyntaxNode) && (n.IsAbstract || n.BaseType == typeof(SyntaxNode))).OrderBy(n => n.Name);
+
+            foreach (var node in roots)
+            {
+                PrintProductionName(node);
+
+                if (!node.IsAbstract)
+                    PrintProduction(node);
+
+                var derivedTypes = typesByBaseType[node];
+
+                foreach (var derivedType in derivedTypes)
+                {
+                    if (derivedType.IsAbstract)
+                    {
+                        PrintProductionNameReference(derivedType);
+                    }
+                    else
+                    {
+                        PrintProduction(derivedType);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static string GetProductionName(Type type)
+        {
+            return type.Name.Replace("Syntax", "");
+        }
+
+        private static void PrintProductionName(Type type)
+        {
+            Console.WriteLine("{0}:", GetProductionName(type));
+        }
+
+        private static void PrintProductionNameReference(Type type)
+        {
+            Console.WriteLine("\t{0}", GetProductionName(type));
+        }
+
+        private static void PrintProduction(Type type)
+        {
+            var constructor = type.GetConstructors().Single();
+            var names = constructor.GetParameters().Skip(1).Select(GetElementName);
+            var nameList = string.Join(" ", names);
+            Console.WriteLine("\t{0}", nameList);
+        }
+
+        private static string GetElementName(ParameterInfo p)
+        {
+            if (p.ParameterType == typeof (SyntaxToken))
+                return p.Name.Replace("Keyword", "").Replace("Token", "").ToUpper();
+
+            if (typeof (SyntaxNode).IsAssignableFrom(p.ParameterType))
+                return GetProductionName(p.ParameterType);
+
+            return p.Name;
+        }
+    }
+}
