2a0759421657cd1378c1d3ffaf3f00d5b915e9d5
Immo Landwerth <immo@landwerth.net>
2015-09-12 09:50:18 -0700

    Add legacy tests

---
+++ src/NQuery.Tests/Legacy/LegacyTests.TestDefinition.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+using NQuery.Text;
+
+namespace NQuery.Tests.Legacy
+{
+    partial class LegacyTests
+    {
+        private sealed class TestDefinition
+        {
+            public TestDefinition(string commandText, string expectedRuntimeError, ImmutableArray<Diagnostic> expectedDiagnostics, DataTable expectedResults)
+            {
+                CommandText = commandText;
+                ExpectedRuntimeError = expectedRuntimeError;
+                ExpectedDiagnostics = expectedDiagnostics;
+                ExpectedResults = expectedResults;
+            }
+
+            public string CommandText { get; private set; }
+
+            public string ExpectedRuntimeError { get; private set; }
+
+            public ImmutableArray<Diagnostic> ExpectedDiagnostics { get; private set; }
+
+            public DataTable ExpectedResults { get; private set; }
+
+            public static TestDefinition FromResource(string name)
+            {
+                var resourceName = GetFullName(name);
+                var assembly = typeof(TestDefinition).Assembly;
+                using (var testDefinitionStream = assembly.GetManifestResourceStream(resourceName))
+                {
+                    if (testDefinitionStream == null)
+                        return null;
+
+                    var testDefinitionXml = new XmlDocument();
+                    testDefinitionXml.Load(testDefinitionStream);
+                    return FromXml(testDefinitionXml);
+                }
+            }
+
+            public static IEnumerable<string> GetResourceNames()
+            {
+                var assembly = typeof(TestDefinition).Assembly;
+                return assembly.GetManifestResourceNames()
+                               .Where(n => n.StartsWith("NQuery.Tests.Legacy."))
+                               .Select(GetSimpleName);
+            }
+
+            private static string GetSimpleName(string resourceName)
+            {
+                return resourceName.Replace("NQuery.Tests.Legacy.", string.Empty)
+                                   .Replace(".xml", string.Empty);
+            }
+
+            private static string GetFullName(string resourceName)
+            {
+                return "NQuery.Tests.Legacy." + resourceName + ".xml";
+            }
+
+            public static TestDefinition FromXml(XmlDocument xmlDocument)
+            {
+                var commandText = xmlDocument.SelectSingleNode("/test/sql").InnerText;
+
+                var expectedRuntimeErrorNode = xmlDocument.SelectSingleNode("/test/expectedRuntimeError");
+                var expectedRuntimeError = expectedRuntimeErrorNode == null ? null : expectedRuntimeErrorNode.InnerText;
+
+                var errorList = new List<Diagnostic>();
+                var expectedErrorsNode = xmlDocument.SelectSingleNode("/test/expectedErrors");
+                if (expectedErrorsNode != null)
+                {
+                    foreach (XmlNode expectedErrorNode in expectedErrorsNode.SelectNodes("expectedError"))
+                    {
+                        var diagnosticId = (DiagnosticId)Enum.Parse(typeof(DiagnosticId), expectedErrorNode.Attributes["id"].Value);
+                        var errorText = expectedErrorNode.Attributes["text"].Value;
+                        var compilationError = new Diagnostic(new TextSpan(), diagnosticId, errorText);
+                        errorList.Add(compilationError);
+                    }
+                }
+
+                DataTable expectedResults = null;
+                var expectedResultsNode = xmlDocument.SelectSingleNode("/test/expectedResults");
+
+                if (expectedResultsNode != null)
+                {
+                    using (var stringReader = new StringReader(expectedResultsNode.InnerXml))
+                    {
+                        var dataSet = new DataSet();
+                        dataSet.ReadXml(stringReader);
+                        expectedResults = dataSet.Tables[0];
+                    }
+                }
+
+                //XmlNode expectedPlanNode = xmlDocument.SelectSingleNode("/test/expectedPlan");
+                //if (expectedPlanNode != null)
+                //{
+                //    result.ExpectedPlan = ShowPlan.FromXml(expectedPlanNode);
+                //}
+
+                return new TestDefinition(commandText, expectedRuntimeError, errorList.ToImmutableArray(), expectedResults);
+            }
+        }
+    }
+}
---
+++ src/NQuery.Tests/Legacy/LegacyTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
+
+using NQuery.Data;
+
+using Xunit;
+
+namespace NQuery.Tests.Legacy
+{
+    public partial class LegacyTests
+    {
+        [Theory]
+        [MemberData("GetTestNames")]
+        public void LegacyTest_Passes(string name)
+        {
+            var testDefinition = TestDefinition.FromResource(name);
+            var dataContext = NorthwindDataContext.Instance;
+            var query = Query.Create(dataContext, testDefinition.CommandText);
+
+            if (testDefinition.ExpectedResults != null)
+            {
+                var actualResults = query.ExecuteDataTable();
+                AssertEqual(testDefinition.ExpectedResults, actualResults);
+            }
+            else if (testDefinition.ExpectedDiagnostics.Any())
+            {
+                var actualException = Assert.Throws<CompilationException>(() => query.ExecuteDataTable());
+                AssertEqual(testDefinition.ExpectedDiagnostics, actualException.Diagnostics);
+            }
+            else if (testDefinition.ExpectedRuntimeError != null)
+            {
+                var actualException = Assert.Throws<Exception>(() => query.ExecuteDataTable());
+                Assert.Equal(testDefinition.ExpectedRuntimeError, actualException.Message);
+            }
+        }
+
+        private static void AssertEqual(DataTable expectedResults, DataTable actualResults)
+        {
+            if (expectedResults == null)
+            {
+                Assert.Null(actualResults);
+                return;
+            }
+
+            Assert.NotNull(actualResults);
+            Assert.Equal(expectedResults.Rows.Count, actualResults.Rows.Count);
+            Assert.Equal(expectedResults.Columns.Count, actualResults.Columns.Count);
+
+            for (var rowIndex = 0; rowIndex < expectedResults.Rows.Count; rowIndex++)
+            {
+                var expectedRow = expectedResults.Rows[rowIndex];
+                var actualRow = actualResults.Rows[rowIndex];
+
+                for (var colIndex = 0; colIndex < expectedResults.Columns.Count; colIndex++)
+                {
+                    var expectedValue = expectedRow[colIndex];
+                    var actualValue = actualRow[colIndex];
+
+                    AssertEqual(expectedValue, actualValue);
+                }
+            }
+        }
+
+        private static void AssertEqual(object expectedValue, object actualValue)
+        {
+            var expectedBytes = expectedValue as byte[];
+            var actualBytes = actualValue as byte[];
+            if (expectedBytes != null && actualBytes != null)
+                AssertEqual(expectedBytes, actualBytes);
+            else
+                Assert.Equal(expectedValue, actualValue);
+        }
+
+        public static void AssertEqual(byte[] expectedValue, byte[] actualValue)
+        {
+            var expectedBase64String = Convert.ToBase64String(expectedValue);
+            var actualBase64String = Convert.ToBase64String(actualValue);
+            Assert.Equal(expectedBase64String, actualBase64String);
+        }
+
+        private static void AssertEqual(ImmutableArray<Diagnostic> expected, ImmutableArray<Diagnostic> actual)
+        {
+            Assert.Equal(expected.Length, actual.Length);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                //Assert.Equal(expected[i].Span, actual[i].Span);
+                Assert.Equal(expected[i].DiagnosticId, actual[i].DiagnosticId);
+                Assert.Equal(expected[i].Message, actual[i].Message);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestNames()
+        {
+            return TestDefinition.GetResourceNames()
+                                 .Select(n => new object[] { n });
+        }
+    }
+}
