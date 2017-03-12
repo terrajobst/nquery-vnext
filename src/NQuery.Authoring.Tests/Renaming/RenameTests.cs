674af93849c650ef74f9b1f39cb3fdbd2e05d2d7
Immo Landwerth <immo@landwerth.net>
2017-03-11 20:44:27 -0800

    Add basic support for rename operations

---
+++ src/NQuery.Authoring.Tests/Renaming/RenameTests.cs
@@ -0,0 +1,413 @@
+﻿using System.Threading.Tasks;
+using NQuery.Authoring.Renaming;
+using NQuery.Text;
+using Xunit;
+
+namespace NQuery.Authoring.Tests.Renaming
+{
+    public class RenameTests
+    {
+        private static Task<RenamedDocument> Create(string queryWithEdit)
+        {
+            var annotatedText = AnnotatedText.Parse(queryWithEdit);
+            Assert.Empty(annotatedText.Spans);
+            var change = Assert.Single(annotatedText.Changes);
+            var query = annotatedText.Text;
+
+            var text = SourceText.From(query);
+            var workspace = new Workspace(text.Container)
+            {
+                DataContext = NorthwindDataContext.Instance,
+                DocumentKind = DocumentKind.Query
+            };
+            return RenamedDocument.CreateAsync(workspace.CurrentDocument, change);
+        }
+
+        private async Task AssertIsMatch(string queryWithEdit, string expectedQueryWithSpans)
+        {
+            var result = await Create(queryWithEdit.NormalizeCode());
+            var expectedQuery = expectedQueryWithSpans.NormalizeCode().ParseSpans(out var expectedSpans);
+
+            Assert.True(result.IsRenamed);
+            Assert.Equal(expectedQuery, result.Document.Text.GetText());
+            Assert.Equal(expectedSpans, result.Spans);
+        }
+
+        private async Task AssertIsInvalid(string queryWithEdit)
+        {
+            var result = await Create(queryWithEdit.NormalizeCode());
+            Assert.False(result.IsRenamed);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_DoesNotRename_WhenInWhitespace()
+        {
+            var query = @"
+                SELECT  e.EmployeeID,{: -- Added comment}
+                        e.LastName
+                FROM    Employees e
+            ";
+
+            await AssertIsInvalid(query);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_DoesNotRename_WhenAddingComma()
+        {
+            var query = @"
+                SELECT  e.EmployeeID,
+                        e.LastName{:,}
+                FROM    Employees e
+            ";
+
+            await AssertIsInvalid(query);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_DoesNotRename_WhenInsertingWhitespace()
+        {
+            var query = @"
+                SELECT  emp.EmployeeID,
+                        emp.LastName
+                FROM    Employees em{: }p
+            ";
+
+            await AssertIsInvalid(query);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_WhenPrefixing()
+        {
+            var query = @"
+                SELECT  p.EmployeeID,
+                        p.LastName
+                FROM    Employees {:em}p
+            ";
+
+            var expected = @"
+                SELECT  {emp}.EmployeeID,
+                        {emp}.LastName
+                FROM    Employees {emp}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_WhenInserting()
+        {
+            var query = @"
+                SELECT  ep.EmployeeID,
+                        ep.LastName
+                FROM    Employees e{:m}p
+            ";
+
+            var expected = @"
+                SELECT  {emp}.EmployeeID,
+                        {emp}.LastName
+                FROM    Employees {emp}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_WhenReplacing()
+        {
+            var query = @"
+                SELECT  x.EmployeeID,
+                        x.LastName
+                FROM    Employees {x:emp}
+            ";
+
+            var expected = @"
+                SELECT  {emp}.EmployeeID,
+                        {emp}.LastName
+                FROM    Employees {emp}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_WhenSuffixing()
+        {
+            var query = @"
+                SELECT  e.EmployeeID,
+                        e.LastName
+                FROM    Employees e{:mp}
+            ";
+
+            var expected = @"
+                SELECT  {emp}.EmployeeID,
+                        {emp}.LastName
+                FROM    Employees {emp}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_WhenDeletingAtStart()
+        {
+            var query = @"
+                SELECT  emp.EmployeeID,
+                        emp.LastName
+                FROM    Employees {em:}p
+            ";
+
+            var expected = @"
+                SELECT  {p}.EmployeeID,
+                        {p}.LastName
+                FROM    Employees {p}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_WhenDeletingAtEnd()
+        {
+            var query = @"
+                SELECT  emp.EmployeeID,
+                        emp.LastName
+                FROM    Employees e{mp:}
+            ";
+
+            var expected = @"
+                SELECT  {e}.EmployeeID,
+                        {e}.LastName
+                FROM    Employees {e}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_DoesNotRename_Table()
+        {
+            var query = @"
+                SELECT  manager.LastName,
+                        report.LastName
+                FROM    Employ{ees:xx} manager
+                            INNER JOIN Employees report ON report.ReportsTo = manager.EmployeeId
+            ";
+
+            await AssertIsInvalid(query);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_DoesNotRename_Column()
+        {
+            var query = @"
+                SELECT  e.EmployeeID,
+                        e.Employee{ID:XX}
+                FROM    Employees e
+            ";
+
+            await AssertIsInvalid(query);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_TableInstance()
+        {
+            var query = @"
+                SELECT  e.EmployeeID,
+                        e.LastName
+                FROM    Employees e{:2}
+            ";
+
+            var expected = @"
+                SELECT  {e2}.EmployeeID,
+                        {e2}.LastName
+                FROM    Employees {e2}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_CommonTableExpression()
+        {
+            var query = @"
+                WITH LondonEmps AS
+                (
+                    SELECT  *
+                    FROM    Employees e
+                    WHERE   e.City = 'London'
+                )
+                SELECT  *
+                FROM    LondonEmps{:2} l
+            ";
+
+            var expected = @"
+                WITH {LondonEmps2} AS
+                (
+                    SELECT  *
+                    FROM    Employees e
+                    WHERE   e.City = 'London'
+                )
+                SELECT  *
+                FROM    {LondonEmps2} l
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_CommonTableExpression_WhenRecursive()
+        {
+            var query = @"
+                WITH RECURSIVE EmpHierarchy AS
+                (
+                    SELECT  e.EmployeeID,
+                            e.FirstName,
+                            e.LastName,
+                            0 AS Level,
+                            e.EmployeeID.ToString() AS Path
+                    FROM    Employees e
+                    WHERE   e.ReportsTo IS NULL
+
+                    UNION ALL
+
+                    SELECT  e.EmployeeID,
+                            e.FirstName,
+                            e.LastName,
+                            eh.Level + 1,
+                            eh.Path + '.' + e.EmployeeID
+                    FROM    Employees e
+                                INNER JOIN EmpHierarchy eh ON eh.EmployeeID = e.ReportsTo
+                )
+                SELECT  SPACE(8 * e.Level) + e.FirstName + ' ' + e.LastName
+                FROM    EmpHierarchy{:2} e
+                ORDER   BY e.Path
+            ";
+
+            var expected = @"
+                WITH RECURSIVE {EmpHierarchy2} AS
+                (
+                    SELECT  e.EmployeeID,
+                            e.FirstName,
+                            e.LastName,
+                            0 AS Level,
+                            e.EmployeeID.ToString() AS Path
+                    FROM    Employees e
+                    WHERE   e.ReportsTo IS NULL
+
+                    UNION ALL
+
+                    SELECT  e.EmployeeID,
+                            e.FirstName,
+                            e.LastName,
+                            eh.Level + 1,
+                            eh.Path + '.' + e.EmployeeID
+                    FROM    Employees e
+                                INNER JOIN {EmpHierarchy2} eh ON eh.EmployeeID = e.ReportsTo
+                )
+                SELECT  SPACE(8 * e.Level) + e.FirstName + ' ' + e.LastName
+                FROM    {EmpHierarchy2} e
+                ORDER   BY e.Path
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact]
+        public async Task RenamedDocument_Renames_ColumnInstance()
+        {
+            var query = @"
+                SELECT  e.FirstName + ' ' + e.LastName AS FullName
+                FROM    Employees e
+                ORDER   BY FullName{:2}
+            ";
+
+            var expected = @"
+                SELECT  e.FirstName + ' ' + e.LastName AS {FullName2}
+                FROM    Employees e
+                ORDER   BY {FullName2}
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact(Skip = "Not implemented yet")]
+        public async Task RenamedDocument_Renames_CommonTableExpression_ExplicitColumn()
+        {
+            var query = @"
+                WITH LondonEmps (EmployeeID, FirstName, LastName) AS
+                (
+                    SELECT  EmployeeID, FirstName, LastName
+                    FROM    Employees e
+                    WHERE   e.City = 'London'
+                )
+                SELECT  l.First{Name:}
+                FROM    LondonEmps l
+            ";
+
+            var expected = @"
+                WITH LondonEmps (EmployeeID, {First}, LastName) AS
+                (
+                    SELECT  EmployeeID, FirstName, LastName
+                    FROM    Employees e
+                    WHERE   e.City = 'London'
+                )
+                SELECT  l.{First}
+                FROM    LondonEmps l
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact(Skip = "Not implemented yet")]
+        public async Task RenamedDocument_Renames_CommonTableExpression_ImplicitColumn()
+        {
+            var query = @"
+                WITH LondonEmps AS
+                (
+                    SELECT  EmployeeID, FirstName AS FirstName, LastName
+                    FROM    Employees e
+                    WHERE   e.City = 'London'
+                )
+                SELECT  l.First{Name:}
+                FROM    LondonEmps l
+            ";
+
+            var expected = @"
+                WITH LondonEmps AS
+                (
+                    SELECT  EmployeeID, FirstName AS {First}, LastName
+                    FROM    Employees e
+                    WHERE   e.City = 'London'
+                )
+                SELECT  l.{First}
+                FROM    LondonEmps l
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+
+        [Fact(Skip = "Not implemented yet")]
+        public async Task RenamedDocument_Renames_DerivedTableColumn()
+        {
+            var query = @"
+                SELECT  l.FullName
+                FROM    (
+                            SELECT  e.FirstName + ' ' + e.LastName AS FullName{:2}
+                            FROM    Employees e
+                            WHERE   e.City = 'London'
+                        ) l
+            ";
+
+            var expected = @"
+                SELECT  l.{FullName2}
+                FROM    (
+                            SELECT  e.FirstName + ' ' + e.LastName AS {FullName2}
+                            FROM    Employees e
+                            WHERE   e.City = 'London'
+                        ) l
+            ";
+
+            await AssertIsMatch(query, expected);
+        }
+    }
+}
---
+++ src/NQuery.Authoring/Renaming/RenamedDocument.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using NQuery.Authoring.SymbolSearch;
+using NQuery.Text;
+
+namespace NQuery.Authoring.Renaming
+{
+    public sealed class RenamedDocument
+    {
+        private RenamedDocument(Document document)
+        {
+            IsRenamed = false;
+            Document = Document;
+            EditSpan = default(TextSpan);
+            OtherSpans = ImmutableArray<TextSpan>.Empty;
+        }
+
+        private RenamedDocument(Document document, TextSpan editSpan, ImmutableArray<TextSpan> otherSpans)
+        {
+            IsRenamed = true;
+            Document = document;
+            EditSpan = editSpan;
+            OtherSpans = otherSpans;
+        }
+
+        public static async Task<RenamedDocument> CreateAsync(Document document, TextChange change)
+        {
+            var semanticModel = await document.GetSemanticModelAsync();
+
+            if (!TryApplyIdentifierEdit(semanticModel.SyntaxTree, change, out var token, out var newSpan, out var newIdentifer))
+                return new RenamedDocument(document);
+
+            var symbolSpan = semanticModel.FindSymbol(token.Span.Start);
+            if (symbolSpan == null)
+                return new RenamedDocument(document);
+
+            var symbolSpans = semanticModel.FindUsages(symbolSpan.Value.Symbol).ToImmutableArray();
+            if (!symbolSpans.Any(ss => ss.Kind == SymbolSpanKind.Definition))
+                return new RenamedDocument(document);
+
+            var resultingChanges = symbolSpans.Select(ss => new TextChange(ss.Span, newIdentifer)).ToImmutableArray();
+
+            var changeSet = TextChangeSet.Create(resultingChanges);
+
+            var newLength = newIdentifer.Length;
+            var allSpans = symbolSpans.Select(ss => new TextSpan(changeSet.TranslatePosition(ss.Span.Start), newLength)).ToImmutableArray();
+            var editSpan = new TextSpan(changeSet.TranslatePosition(symbolSpan.Value.Span.Start), newLength);
+            var otherSpans = allSpans.Where(s => s != editSpan).ToImmutableArray();
+
+            var newText = document.Text.WithChanges(resultingChanges);
+            var newDocument = document.WithText(newText);
+
+            return new RenamedDocument(newDocument, editSpan, otherSpans);
+        }
+
+        private static bool TryApplyIdentifierEdit(SyntaxTree syntaxTree, TextChange change, out SyntaxToken token, out TextSpan newSpan, out string newIdentifier)
+        {
+            token = null;
+            newSpan = default(TextSpan);
+            newIdentifier = null;
+
+            token = FindOverlappingIdentifierOrKeyword(syntaxTree, change.Span);
+            if (token == null)
+                return false;
+
+            var delta = change.NewText.Length - change.Span.Length;
+
+            var editStart = change.Span.Start;
+            var start = Math.Min(editStart, token.Span.Start);
+            var end = token.Span.End + delta;
+
+            var newText = syntaxTree.Text.WithChanges(change);
+            newSpan = TextSpan.FromBounds(start, end);
+            newIdentifier = newText.GetText(newSpan);
+
+            return SyntaxFacts.IsValidIdentifierOrKeyword(newIdentifier);
+        }
+
+        private static SyntaxToken FindOverlappingIdentifierOrKeyword(SyntaxTree syntaxTree, TextSpan span)
+        {
+            var tokenAtStart = syntaxTree.Root.FindToken(span.Start);
+            var tokenAtEnd = syntaxTree.Root.FindToken(span.End);
+            var tokenBeforeEnd = tokenAtEnd.GetPreviousToken();
+
+            // Case 1: Token contains span
+            //
+            //     [Span]
+            // [----Token----]
+
+            if (tokenAtStart == tokenAtEnd && tokenAtStart.Span.IntersectsWith(span) && tokenAtStart.Kind.IsIdentifierOrKeyword())
+                return tokenAtStart;
+
+            // Case 2: Token starts at end
+            //
+            // [Span]
+            //      [----Token----]
+
+            if (span.End == tokenAtEnd.Span.Start && tokenAtEnd.Kind.IsIdentifierOrKeyword())
+                return tokenAtEnd;
+
+            // Case 3: Beginning of token overlaps with span
+            //
+            // [Span]
+            //    [----Token----]
+
+            if (tokenAtEnd.Span.IntersectsWith(span) && tokenAtEnd.Kind.IsIdentifierOrKeyword())
+                return tokenAtEnd;
+
+            // Case 4: Token ends at start
+            //
+            //               [Span]
+            // [----Token----]
+
+            if (tokenBeforeEnd != null && tokenBeforeEnd.Span.End == span.Start && tokenBeforeEnd.Kind.IsIdentifierOrKeyword())
+                return tokenBeforeEnd;
+
+            // Case 5: End of token overlaps with span
+            //
+            //             [Span]
+            // [----Token----]
+
+            if (tokenAtStart.Span.IntersectsWith(span) && tokenAtStart.Kind.IsIdentifierOrKeyword())
+                return tokenAtStart;
+
+            return null;
+        }
+
+        public bool IsRenamed { get; }
+
+        public Document Document { get; }
+
+        public TextSpan EditSpan { get; }
+
+        public ImmutableArray<TextSpan> OtherSpans { get; }
+
+        public IEnumerable<TextSpan> Spans => new[] { EditSpan }.Concat(OtherSpans).OrderBy(s => s.Start);
+    }
+}
