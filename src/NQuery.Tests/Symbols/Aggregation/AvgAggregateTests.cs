0e2ecd5b31fb5a239b193c56ede7fc44e03ace2d
Thomas Dallmair <thomasd@theprojectgroup.com>
2017-09-19 16:56:18 +0200

    Fix InvalidCastException when AVG aggregate needs to provide a result although it hasn't seen any rows.

---
+++ src/NQuery.Tests/Symbols/Aggregation/AvgAggregateTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using NQuery.Symbols.Aggregation;
+
+using Xunit;
+
+namespace NQuery.Tests.Symbols.Aggregation
+{
+    public sealed class AvgAggregateTests : AggregateTests
+    {
+        [Fact]
+        public void Aggregates_Avg_ReturnsNull_IfInputIsEmpty()
+        {
+            var values = new object[0];
+            AssertProduces(null, typeof(int), values);
+        }
+
+        protected override AggregateDefinition CreateAggregateDefinition()
+        {
+            return new AvgAggregateDefinition();
+        }
+    }
+}
