8c3f5b5013bf3871615abc3ceb18ef751a2a5788
Immo Landwerth <immol@microsoft.com>
2017-09-20 15:56:22 -0700

    Use MSBuild 15

--- build.cmd
+++ build.cmd
@@ -1,5 +1,10 @@
 @echo off
+setlocal
 
+:: Find MSBuild to use
+
+for /f "tokens=*" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath') do set VSPATH=%%i
+set MSBUILD_PATH=%VSPATH%\MSBuild\15.0\Bin\MSBuild.exe
 
 :: Note: We've disabled node reuse because it causes file locking issues.
 ::       The issue is that we extend the build with our own targets which
@@ -7,4 +12,4 @@
 ::       assembly.
 
 if not exist bin mkdir bin
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" /nologo /m /v:m /nr:false /flp:verbosity=normal;LogFile=bin\msbuild.log %*
+"%MSBUILD_PATH%" /nologo /m /v:m /nr:false /flp:verbosity=normal;LogFile=bin\msbuild.log %*
