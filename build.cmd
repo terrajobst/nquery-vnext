5172e1afbb6e8647d9b2868b66eceb692f6adf2e
Immo Landwerth <immol@microsoft.com>
2017-09-20 15:55:36 -0700

    Move mkdir under note

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-if not exist bin mkdir bin
 
 :: Note: We've disabled node reuse because it causes file locking issues.
 ::       The issue is that we extend the build with our own targets which
 ::       means that that rebuilding cannot successfully delete the task
 ::       assembly.
 
+if not exist bin mkdir bin
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" /nologo /m /v:m /nr:false /flp:verbosity=normal;LogFile=bin\msbuild.log %*
