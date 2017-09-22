1916ec632c911356e73cb5a5b8f8a8c4bc6ec16f
Immo Landwerth <immol@microsoft.com>
2017-09-21 18:14:36 -0700

    Centralize properties via Directory.Build.targets

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -2,10 +2,6 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly : AssemblyCopyright("Copyright © Immo Landwerth")]
-[assembly : AssemblyCompany("Immo Landwerth")]
-[assembly : AssemblyProduct("NQuery")]
-
 [assembly : AssemblyMetadata("ProjectUrl", "http://nquery.codeplex.com")]
 [assembly : AssemblyMetadata("LicenseUrl", "http://nquery.codeplex.com/license")]
 
