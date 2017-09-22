edbe15f753b6c4342840647d6bef71c051917e53
Immo Landwerth <immol@microsoft.com>
2017-09-21 19:30:04 -0700

    Replace NuProj with built-in package generation

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -2,8 +2,5 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly : AssemblyMetadata("ProjectUrl", "http://nquery.codeplex.com")]
-[assembly : AssemblyMetadata("LicenseUrl", "http://nquery.codeplex.com/license")]
-
 [assembly : CLSCompliant(false)]
 [assembly : ComVisible(false)]
