9ff4ecc3d18937a55ad68db3a53b424ac5622e43
Immo Landwerth <immo@landwerth.net>
2022-03-06 02:41:55 -0800

    Fix UnusedValueSlotRemover such that it doesn't eliminate outer references
    For example, in this snippet
    
        SELECT  (
                    SELECT  COUNT(*)
                    FROM    EmployeeTerritories et
                    WHERE   et.EmployeeID = e.EmployeeID
                ) AS TerritoryCount
        FROM    Employees e
    
    the table scan for Employees no longer included e.EmployeeID.

---
+++ src/NQuery.Tests/Evaluation/OuterReferencesTests.cs
@@ -0,0 +1,32 @@
+﻿namespace NQuery.Tests.Evaluation
+{
+    public class OuterReferencesTests : EvaluationTest
+    {
+        [Fact]
+        public void OuterReferences_AreNot_Removed()
+        {
+            var text = @"
+                SELECT  (
+                            SELECT  COUNT(*)
+                            FROM    EmployeeTerritories et
+                            WHERE   et.EmployeeID = e.EmployeeID
+                        ) AS TerritoryCount
+                FROM    Employees e
+            ";
+
+            var expected = new[] {
+                2,
+                7,
+                4,
+                3,
+                7,
+                5,
+                10,
+                4,
+                7
+            };
+
+            AssertProduces(text, expected);
+        }
+    }
+}
