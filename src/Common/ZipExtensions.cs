fb2e400a6afe41d10e63de1259f02534307a0934
Immo Landwerth <immo@landwerth.net>
2026-05-31 12:15:10 -0700

    Replace conditional compilation for Zip() with polyfill

---
+++ src/Common/ZipExtensions.cs
@@ -0,0 +1,21 @@
+#if NETFRAMEWORK
+
+using System.Collections.Generic;
+
+#nullable enable
+
+namespace System.Linq
+{
+    internal static class ZipExtensions
+    {
+        extension<TFirst, TSecond>(IEnumerable<TFirst> first)
+        {
+            public IEnumerable<(TFirst First, TSecond Second)> Zip(IEnumerable<TSecond> second)
+            {
+                return first.Zip(second, static (f, s) => (f, s));
+            }
+        }
+    }
+}
+
+#endif
