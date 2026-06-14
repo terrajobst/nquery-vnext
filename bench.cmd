34cae83d2b88d5bf9b1af1871a3c0562cedac70c
Immo Landwerth <immo@landwerth.net>
2026-06-13 19:56:13 -0700

    Fix benchmarks

--- bench.cmd
+++ bench.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 
-set PROJECT_FILE=%~dp0src\Benchmarks\NQuery.Benchmarks\NQuery.Benchmarks.csproj
+set PROJECT_FILE=%~dp0src\NQuery.Benchmarks\NQuery.Benchmarks.csproj
 dotnet run --project %PROJECT_FILE% -c Release -- %*
